91f46356984377d074e3789bcfd032b77f18a2d3
forhadhossen1 <mr.forhad1030@gamil.com>
2023-09-02 18:21:02 +0600

    completed all topic

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const handleCategory = async () => {
-    const response = await fetch(` https://openapi.programming-hero.com/api/videos/categories`);
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
     const data = await response.json();
     const videos = data.data;
-    // console.log(videos);
+    //console.log(videos)
 
     const tabContainer = document.getElementById('tab-container');
     videos.slice(0, 4).forEach(category => {
         const div = document.createElement('div');
         div.innerHTML = `
-            <button onclick = "handleVideos('${category.category_id}')" class="btn px-6">${category.category}</button>
+            <button onclick="handleVideos('${category.category_id}')" class="btn px-6">${category.category}</button>
        `;
 
         tabContainer.appendChild(div);
@@ -17,14 +17,24 @@ const handleCategory = async () => {
 
 const handleVideos = async (categoryId) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
-    // console.log(categoryId);
     const data = await response.json();
     const allData = data.data;
-    console.log(allData);
+    // console.log(allData);
+    
+    document.getElementById('sort-btn').addEventListener('click', function () {
+        allData.sort((a, b) => {
+            const viewsA = parseFloat(a.others.views);
+            const viewsB = parseFloat(b.others.views);
+            return viewsB - viewsA;
+        });
+        console.log('Data sorted:', allData);
+        displayVideos(allData);
+    });
 
+    displayVideos(allData);
+};
 
-
-
+const displayVideos = (allData) => {
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = '';
     const noContainer = document.getElementById('no-video');
@@ -34,71 +44,54 @@ const handleVideos = async (categoryId) => {
         const noVideo = document.createElement('div');
         noVideo.innerHTML = `
         <div class="text-center items-center">
-        <div class="flex justify-center py-5"><img src="Icon.png" alt="no video worning"></div>
-        <h2 class = "text-3xl py-4 font-bold">Oops !! Sorry, There is no <br> content here</h2>
+        <div class="flex justify-center py-5"><img src="Icon.png" alt="no video warning"></div>
+        <h2 class="text-3xl py-4 font-bold">Oops !! Sorry, There is no <br> content here</h2>
     </div>
         `;
         noContainer.appendChild(noVideo);
-    }
-
-
-
-    else {
+    } else {
         allData.forEach((video) => {
-            console.log(video?.others?.posted_date);
             const div = document.createElement('div');
             div.innerHTML = `
         <div class="card bg-base-100">
-
         <figure>
-            <img src="${video?.thumbnail}" alt="video thumble" class="rounded-lg h-72 w-full" />
+            <img src="${video?.thumbnail}" alt="video thumbnail" class="rounded-lg h-72 w-full" />
             ${video?.others?.posted_date ? `
             <div class="absolute bottom-0 mb-40 right-3 rounded-lg bg-gray-600">
             <p class="text-center">${convertToHoursMinutes(video.others.posted_date)}</p>
             </div>` : ''}
         </figure>
-
-
-
-    <div class="flex">
-        <div class="py-3 px-2">
-            <img src="${video?.authors[0]?.profile_picture || ''}" alt="" class="w-12 h-12 rounded-full">
-        </div>
-
-        
-        <div class="">
-            <h2 class="card-title py-3">${video?.title}</h2>
-            <h4 class="text-xl py-3">
-            ${video?.authors[0]?.profile_name || ''}
-
-            <div class="${video?.authors[0]?.verified ? 'badge badge-primary h-5 w-5 rounded-full' : ''}">
-            ${video?.authors[0]?.verified ? '✓ ' : ''}
-          </div>
-            </h4>
-
+        <div class="flex">
+            <div class="py-3 px-2">
+                <img src="${video?.authors[0]?.profile_picture || ''}" alt="" class="w-12 h-12 rounded-full">
+            </div>
             <div>
-                <h5 class="text-left py-3">${video?.others?.views ? video.others.views : " "} views
-                </h5>
+                <h2 class="card-title py-3">${video?.title}</h2>
+                <h4 class="text-xl py-3">
+                    ${video?.authors[0]?.profile_name || ''}
+                    <div class="${video?.authors[0]?.verified ? 'badge badge-primary h-5 w-5 rounded-full' : ''}">
+                        ${video?.authors[0]?.verified ? '✓ ' : ''}
+                    </div>
+                </h4>
+                <div>
+                    <h5 class="text-left py-3">${video?.others?.views ? video.others.views : " "} views</h5>
+                </div>
             </div>
         </div>
     </div>
-</div>
         `;
             cardContainer.appendChild(div);
         });
     }
-}
+};
+
+const convertToHoursMinutes = (postedDate) => {
+    const totalSeconds = Math.floor(postedDate);
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const timeString = `${hours}hrs ${minutes}min ago`;
+    return timeString;
+};
 
 handleCategory();
 handleVideos("1000");
-
-convertToHoursMinutes = (postedDate) => {
-    const totalSeconds = Math.floor(postedDate);
-
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-
-    const timeString = `${hours}hrs ${minutes}min ago`;
-
-    return timeString;
-}
